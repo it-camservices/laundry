2f60c125a6a5a20f401195265d5cfe13786be40f
yavsophearin <yavsophearin@gmail.com>
2013-10-21 14:23:52 +0700

    Delete all

--- include/footer-info.php
+++
@@ -1,6 +0,0 @@
-<div class="ui-grid-c ui-responsive">
-            <div class="ui-block-a"><div class="ui-body"><p>&nbsp; Copyright 2013 &nbsp;</p></div></div>
-            <div class="ui-block-b"><div class="ui-body"><p>Design By: <a href="itcam.php" data-rel="dialog">it-camservices.net</a></p></div></div>
-            <div class="ui-block-c"><div class="ui-body"><p>Mobite web support</p></div></div>
-            <div class="ui-block-d"><div class="ui-body"><a href="#popupLogin" data-rel="popup" data-position-to="window" data-role="button" data-inline="true" data-icon="check" data-theme="c" data-transition="slidedown">Sign in here</a></div></div>
-        </div><!-- /grid-c -->
