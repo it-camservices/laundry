8e6f2ce1ed0539ff9c361fc3daec7957abb5899f
yavsophearin <yavsophearin@gmail.com>
2013-10-21 14:43:22 +0700

    start new project

---
+++ include/link-header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet"  href="css/themes/default/jquery.mobile-1.3.2.min.css">
+	<link rel="stylesheet" href="_assets/css/jqm-demos.css">
+    <link rel="stylesheet" type="text/css" href="css/mycss.css">
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+    <script src='//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>
+	<script src="js/jquery.js"></script>
+	<script src="_assets/js/index.js"></script>
+	<script src="js/jquery.mobile-1.3.2.min.js"></script>
+	<script src="js/incrementing.js"></script>
---
+++ include/menu-right-panel.php
@@ -0,0 +1,14 @@
+<div data-role="panel" class="jqm-nav-panel jqm-navmenu-panel" data-position="right" data-display="reveal" data-theme="c" id="add-form">
+
+<form class="userform">
+    <h2>Contact Us</h2>
+    <label for="name">Name:</label>
+    <input name="name" id="name" value="" data-clear-btn="true" data-mini="true" type="text">
+    <label for="password">Email:</label>
+    <input name="password" id="password" value="" data-clear-btn="true" autocomplete="off" data-mini="true" type="password">
+    <div class="ui-grid-a">
+        <div class="ui-block-a"><a href="#" data-rel="close" data-role="button" data-theme="c" data-mini="true">Cancel</a></div>
+        <div class="ui-block-b"><a href="#" data-rel="close" data-role="button" data-theme="b" data-mini="true">Save</a></div>
+    </div>
+</form>
+</div>
